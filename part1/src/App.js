8e2b6ab715533ea1e884d753822d3827c5aa3d78
AnushaDeviR <anusha3599@gmail.com>
2023-07-30 11:32:49 +0100

    props part1

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,16 +1,21 @@
-const Hello = () => {
+const Hello = (props) => {
+  console.log(props);
   return (
     <div>
-      <p>Hello</p>
+      <p>
+        Hello {props.name}, you are {props.age} years old.
+      </p>
     </div>
   );
 };
 const App = () => {
   const now = new Date();
-  console.log(now);
+  const name = "Jane Doe";
+  const age = 10;
   return (
     <div>
-      <Hello />
+      <Hello name={"John Doe"} />
+      <Hello name={name} age={age} />
       <p>The time is {now.toString()}</p>
     </div>
   );
