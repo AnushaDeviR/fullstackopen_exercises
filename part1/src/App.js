caad2341460c3ad5b1998e4ebf3e879f9a81f6a1
AnushaDeviR <anusha3599@gmail.com>
2023-07-30 12:06:39 +0100

    objects in react

--- part1/src/App.js
+++ part1/src/App.js
@@ -12,11 +12,22 @@ const App = () => {
   const now = new Date();
   const name = "Jane Doe";
   const age = 10;
+  const friends = [
+    { name: "Peter", age: 4 },
+    { name: "Maya", age: 10 },
+  ];
+
   return (
     <>
       <Hello name={"John Doe"} />
       <Hello name={name} age={age} />
       <p>The time is {now.toString()}</p>
+      <p>
+        {friends[0].name}, {friends[0].age}
+      </p>
+      <p>
+        {friends[1].name}, {friends[1].age}
+      </p>
     </>
   );
 };
