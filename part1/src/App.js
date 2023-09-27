a6928a542260588599ee7cfa11346c1e9d462126
AnushaDeviR <anusha3599@gmail.com>
2023-09-27 12:21:41 +0100

    destructured props parameters

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,15 +1,23 @@
 import { useState } from "react";
 
-const Hello = (props) => {
-  console.log(props);
+const Hello = (name, age) => {
   return (
     <div>
       <p>
-        Hello {props.name}, you are {props.age} years old.
+        Hello {name}, you are {age} years old.
       </p>
     </div>
   );
 };
+
+const Display = (counter) => {
+  return <>{counter}</>;
+};
+
+const Button = (handleClick, text) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
+
 const App = () => {
   const now = new Date();
   const name = "Jane Doe";
@@ -20,21 +28,31 @@ const App = () => {
   ];
   const [counter, setCounter] = useState(0);
 
-  setTimeout(() => setCounter(counter + 1), 1000);
+  // setTimeout(() => setCounter(counter + 1), 1000);
   console.log("rendering...", counter);
+
+  const incrementCounter = () => setCounter(counter + 1);
+  const decrementCounter = () => setCounter(counter - 1);
+  const resetCounter = () => setCounter(0);
 
   return (
     <>
-      <Hello name={"John Doe"} />
       <Hello name={name} age={age} />
-      <p>The time is {now.toString()}</p>
-      <p>
+
+      {/* <p>
         {friends[0].name}, {friends[0].age}
       </p>
       <p>
         {friends[1].name}, {friends[1].age}
-      </p>
-      <p>{counter}</p>
+      </p> */}
+
+      <Display counter={counter} />
+      <br />
+      <Button handleClick={incrementCounter} text="Increment" />
+      <Button handleClick={decrementCounter} text="Decrement" />
+      <Button handleClick={resetCounter} text="Reset" />
+
+      <p>Timestamp: {now.toString()}</p>
     </>
   );
 };
